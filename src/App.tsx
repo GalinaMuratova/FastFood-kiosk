e5d48b8a5eb9269ec56845408f98f1fbfa5c59b9
GalinaMuratova <ufkz200147@gmail.com>
2023-06-03 13:03:38 +0600

    Реализовала Панель справа (Add items). По клику на блюдо добавляется в детали заказа.

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,15 @@ import React, {useState} from 'react';
 import './App.css';
 import Order from "./Order/Order";
 import TotalPrice from "./TotalPrice/TotalPrice";
+import Items from "./Items/Items";
 
 function App() {
   const [items, setItems] = useState([
     {name: 'Hamburger', count: 0, price: 80},
-    {name: 'Cheeseburger', count: 0, price: 90},
+    {name: 'Cheeseburger', count: 1, price: 90},
     {name: 'Fries', count: 0, price: 60},
     {name: 'Coffee', count: 0, price: 70},
-    {name: 'Tea', count: 0, price: 50},
+    {name: 'Tea', count: 1, price: 50},
     {name: 'Cola', count: 0, price: 40}
   ]);
 
@@ -21,12 +22,28 @@ function App() {
     return total;
   };
 
+  const addItem = (index:number) => {
+    const copyItems = [...items];
+    const itemCopy = {...items[index]};
+    itemCopy.count += 1;
+    copyItems[index] = itemCopy;
+    setItems(copyItems);
+  }
+
+
   return (
     <div className="App">
-      {items.map(item=> (
-          <Order name={item.name} count={item.count} price={item.price}/>
-      ))}
-      <TotalPrice total={totalPrice()}/>
+      <div className="order-details">
+        {items.map((item, index)=> (
+            <Order key={index} name={item.name} count={item.count} price={item.price}/>
+        ))}
+        <TotalPrice total={totalPrice()}/>
+      </div>
+      <div className="items">
+        {items.map((item, index) =>(
+            <Items key={index} name={item.name} price={item.price} onClick={() => addItem(index)}/>
+        ))}
+      </div>
     </div>
   );
 }
--- src/Items/Items.tsx
+++ src/Items/Items.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
 
-const Items = () => {
+interface ItemsProps {
+    name: string;
+    price:number;
+    onClick: React.MouseEventHandler;
+}
+
+const Items:React.FC<ItemsProps> = (props) => {
     return (
-        <div>
-            
+        <div className="dish" onClick={props.onClick}>
+            <div className={props.name + '-img'}></div>
+            <div>
+                <h4>{props.name}</h4>
+                <p>Price:{props.price}</p>
+            </div>
         </div>
     );
 };
